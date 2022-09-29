2ba1318c92b8bd4e2d4b57cb60810aede8416190
aleksandarilic <a.ilic@vegait.rs>
2022-09-29 23:36:38 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env::{args, Args};
+
+//println! - is a macro
+//macros are like functions, but they are expanded before the compilation
 fn main() {
+    let mut args: Args = args();
+    let first = args.nth(0).unwrap();
+    println!("{:?}", first);
     println!("Hello, world!");
 }
